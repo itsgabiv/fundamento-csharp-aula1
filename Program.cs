bc9298f0742822df572af1c7e47659f9f07c1278
itsgabiv <gabi_vee@hotmail.com>
2021-11-26 20:45:58 +0000

    Exercicio 1

--- Program.cs
+++ Program.cs
@@ -1,21 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace dotnetcore
 {
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
     class Program
     {
-        // Exercício 1:
-        // - Fazer a média de notas dos dois alunos (Zuqui, Bronza) e imprimir no Console.WriteLine();
-        // - Imprimir no console para cada aluno
-        //      - Nome do aluno
-        //      - Caso média maior ou igual 7:
-        //          - Passou no ano letivo
-        //          - Não passou no ano letivo
-        // Requisítos:
-        // - Criar um método "CalcularMedia", onde a entrada de parâmetros seja as notas e o retorno seja a média em float
-        // - Utilizar For ou Foreach
-
-
         // Exercício 2:
         // - Fazer um programa que cadastre pessoas, contendo nome e idade;
         // - Imprimir a media de idade das pessoas
@@ -29,8 +24,48 @@ namespace dotnetcore
         {
             Console.WriteLine("Iniciando Programa");
 
-            var xNotasZuqui = new int[] { 7, 6, 5, 3 };
-            var xNotasBronza = new int[] { 8, 9, 10, 8 };
+            var xNotasZuqui = new List<int> { 7, 6, 5, 3 };
+            var xNotasBronza = new List<int> { 8, 9, 10, 8 };
+            var xMediaZuqui = CalcularMedia(xNotasZuqui);
+            Console.WriteLine("Média do Zuqui: " + xMediaZuqui);
+            ImprimirResultado(xMediaZuqui);
+
+
+            var xMediaBronza = CalcularMedia(xNotasBronza);
+            //Console.WriteLine("Média do Bronza: " + CalcularMedia(xNotasBronza));
+            Console.WriteLine("Média do Bronza: " + xMediaBronza);
+            ImprimirResultado(xMediaBronza);
+
+
+            //Método Caclular média
+            float CalcularMedia(List<int> pNotas)
+            {
+                var xMedia = 0;
+                var xSomaNotas = 0;
+                var xQuantidadeItens = 0;
+
+               foreach (var xNota in pNotas)
+               {
+                   xSomaNotas = xSomaNotas + xNota;
+                   xQuantidadeItens = xQuantidadeItens + 1;
+                   xMedia = xSomaNotas / xQuantidadeItens;
+               }
+
+               return xMedia;
+
+            }
+
+            static void ImprimirResultado(float pMedia)
+            {
+                if (pMedia >= 7)
+                {
+                    Console.WriteLine ("Passou no ano letivo");
+                }
+                else
+                {
+                    Console.WriteLine ("Não passou no ano letivo");
+                }
+            }
         }
     }
 }
